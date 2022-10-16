8b2cb1f14da1bd6e87d6510aec99c9dc9a92788a
campbell-lucas <campbell_lucas@outlook.com>
2022-10-16 21:18:14 +0200

    update

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views., name='home'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup')
 ]
