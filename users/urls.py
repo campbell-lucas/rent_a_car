81bd21ed62a1fe6d479031a2c4565e2395094cb9
campbell-lucas <campbell_lucas@outlook.com>
2022-10-20 20:07:43 +0200

    edited urls.py so I can reverse to the urls, added registration view in views.py file

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('signup/', views.signup_view, name='signup')
+    path('signup/', views.SignupView.as_view(), name='signup')
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from . import forms
 
 
 def login_view(request):
     return render(request, 'users/login.html')
 
 
-def signup_view(request):
-    return render(request, 'users/signup.html')
+class SignupView(FormView):
+    form_class = forms.RegistrationForm
+    template_name = 'users/signup.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        if form.is_valid():
+            user = form.save(commit=True)
+            user.is_active = True
+            user.save()
+        return super().form_valid(form)
