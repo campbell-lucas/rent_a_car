3004ab6958237d9ae876efea9dc54722b19cd1f0
campbell-lucas <campbell_lucas@outlook.com>
2022-10-20 20:06:36 +0200

    added forms.py to users app to enable the use of django forms

---
+++ users/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'password')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password'))
+
+        if commit:
+            user.save()
+
+        return user
