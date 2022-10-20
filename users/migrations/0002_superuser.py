90941213fea7bf4222591e03de2134d53c7586b5
campbell-lucas <campbell_lucas@outlook.com>
2022-10-20 18:51:34 +0200

    edited migration file so it has username field

--- users/migrations/0002_superuser.py
+++ users/migrations/0002_superuser.py
@@ -6,8 +6,17 @@ import os
 
 def create_superuser(apps, schema_editor):
     User = get_user_model()
-    
-    
+
+    DJ_SU_EMAIL = os.environ.get('DJ_SU_EMAIL')
+    DJ_SU_USERNAME = os.environ.get('DJ_SU_USERNAME')
+    DJ_SU_PASSWORD = os.environ.get('DJ_SU_PASSWORD')
+
+    User.objects.create_superuser(
+        email=DJ_SU_EMAIL,
+        username=DJ_SU_USERNAME,
+        password=DJ_SU_PASSWORD,
+    )
+
 
 class Migration(migrations.Migration):
 
