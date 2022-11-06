a6660c1e5f7a97b7b192bfca3a2a1f3f49bfcef2
campbell-lucas <campbell_lucas@outlook.com>
2022-11-06 22:00:55 +0100

    added Car model and Category model

--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 
 
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category
+
+
 class Car(models.Model):
-    pass
+    car_brand = models.CharField(max_length=100)
+    car_model = models.CharField(max_length=100)
+    car_color = models.CharField(max_length=100)
+    number_of_seats = models.CharField(max_length=100)
+    number_of_doors = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='car_category')
+
+    def __str__(self):
+        return self.car_model
+
+    def save(self, *args, **kwargs):
+        return super().save(*args, **kwargs)
