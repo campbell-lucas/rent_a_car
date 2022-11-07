85d21856d8d116745b1db23236835c0a5612aefb
campbell-lucas <campbell_lucas@outlook.com>
2022-11-07 21:29:11 +0100

    edited cars forms.py file

--- cars/forms.py
+++ cars/forms.py
@@ -1,2 +1,13 @@
 from django import forms
+from .models import Car
 
+
+class AddCarForm(forms.ModelForm):
+
+    class Meta:
+        model = Car
+        fields = ('car_brand', 'car_model', 'car_color', 'number_of_seats', 'number_of_doors', 'owner', 'category')
+
+    def save(self, commit=True):
+        car = super().save(commit=True)
+        return car
