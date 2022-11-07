9ce26783db78c582dc382c69a40504448da6df53
campbell-lucas <campbell_lucas@outlook.com>
2022-11-07 21:29:53 +0100

    added views to file

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
-# Create your views here.
+from cars import forms
+
+
+def add_car_view(request):
+    form = forms.AddCarForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            car = form.save(commit=False)
+            car.save()
+            return redirect(reverse_lazy('users:profile'))
+    return render(request, 'cars/add_car.html', {'form', form})
