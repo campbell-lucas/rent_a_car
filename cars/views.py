46d35c4b14f966181f9dabecc0b45eae140652e3
campbell-lucas <campbell_lucas@outlook.com>
2022-11-08 18:51:30 +0100

    edited the class view so owner only has the currently logged in user as an option

--- cars/views.py
+++ cars/views.py
@@ -1,14 +1,42 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
 
-from cars import forms
+from cars import forms, models
 
 
-def add_car_view(request):
-    form = forms.AddCarForm(request.POST or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            car = form.save(commit=False)
-            car.save()
-            return redirect(reverse_lazy('users:profile'))
-    return render(request, 'cars/add_car.html', {'form', form})
+class AddCarView(CreateView):
+
+    model = models.Car
+    fields = [
+        'car_brand',
+        'car_model',
+        'car_color',
+        'number_of_seats',
+        'number_of_doors',
+        'category',
+        'owner',
+    ]
+
+    template_name = 'cars/add_car.html'
+    raise_exception = False
+    success_url = reverse_lazy('home:home')
+
+    def get_form(self, *args, **kwargs):
+        form = super(AddCarView, self).get_form(*args, **kwargs)
+        form.fields['owner'].queryset = User.objects.all().filter(username=self.request.user.username)
+        return form
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+# def add_car_view(request):
+#     form = forms.AddCarForm(request.POST or None)
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             car = form.save(commit=False)
+#             car.save()
+#             return redirect(reverse_lazy('users:profile'))
+#     return render(request, 'cars/add_car.html', {'form': form})
