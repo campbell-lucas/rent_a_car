91bfd7b97aebd6f421883cc1771c2ca7482ec775
campbell-lucas <campbell_lucas@outlook.com>
2022-11-16 20:56:39 +0100

    edited home/views.py file with new views

--- home/views.py
+++ home/views.py
@@ -1,5 +1,19 @@
+from django.db.models import Q
 from django.shortcuts import render
+from django.views.generic import ListView
+from cars.models import Car
 
 
 def home_view(request):
     return render(request, 'home/landing-page.html')
+
+
+class SearchResultsView(ListView):
+    model = Car
+    template_name = 'home/search-results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = Car.objects.all().filter(location=query)
+        return object_list
+
