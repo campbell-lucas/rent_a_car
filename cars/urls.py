b3eb7b0bca3e711cb15f2cad24b4e279b237b8c5
campbell-lucas <campbell_lucas@outlook.com>
2022-11-23 22:12:56 +0100

    added car rent url so button will work in detail view

--- cars/urls.py
+++ cars/urls.py
@@ -7,4 +7,5 @@ app_name = 'cars'
 urlpatterns = [
     path('add_car/', views.AddCarView.as_view(), name='add-car'),
     path('car_profile/<int:pk>/', views.CarProfileView.as_view(), name='car-profile'),
+    path('rent_car/<int:pk>/', views.CarRent.as_view(), name='car_rent'),
 ]
