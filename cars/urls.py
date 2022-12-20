856e2564dfc4eb3362621ce8f1c712c6ad5f83c6
campbell-lucas <campbell_lucas@outlook.com>
2022-12-20 20:31:52 +0100

    removed one url which was no longer needed

--- cars/urls.py
+++ cars/urls.py
@@ -7,5 +7,5 @@ app_name = 'cars'
 urlpatterns = [
     path('add_car/', views.AddCarView.as_view(), name='add-car'),
     path('car_profile/<int:pk>/', views.CarProfileView.as_view(), name='car-profile'),
-    path('rent_car/<int:pk>/', views.CarRent.as_view(), name='car_rent'),
+    path('rent_car/<int:pk>/', views.car_rent_view, name='car_rent'),
 ]
